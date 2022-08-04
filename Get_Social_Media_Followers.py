4a99fd7d7e30a2c16358733e29c386d470430ad0
Tyler Kulisz <100548121+Tyler-Kulisz@users.noreply.github.com>
2022-08-04 14:45:39 -0400

    Create Get_Social_Media_Followers.py

---
+++ Get_Social_Media_Followers.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+import time
+
+
+#gets tikok followers of url
+def getTikTokFollowers(driver):
+
+    # get url and check for the request
+    URL = '#TIK TOK PAGE URL'
+
+    driver.get(URL)
+    time.sleep(4)
+    followers_tiktok = driver.find_element_by_xpath('#XPATH FOR TIKTOK PAGE')
+
+    return int(followers_tiktok.text)
+
+def getInstaFollowers(driver):
+
+    # get url and check for the request
+    URL2 = '#INSTAGRAM PAGE URL'
+
+    driver.get(URL2)
+    time.sleep(4)
+    followers_insta = driver.find_element_by_xpath('#XPATH FOR INSTAGRAM PAGE')
+
+    return int(followers_insta.text)
+
+def getTwitterFollowers(driver):
+
+    # get url and check for the request
+    URL3 = '#Twitter PAGE URL'
+
+    driver.get(URL3)
+    time.sleep(4)
+
+    followers_twitter = driver.find_element_by_xpath\
+        ('#XPATH FOR TWITTER PAGE')
+
+    followers_value_twitter = followers_twitter.text
+    followers_value_twitter_new = followers_value_twitter.replace(',','')
+
+    return int(followers_value_twitter_new)
+
+
+def main():
+
+    file_path = r"#ABSOLUTE PATH OF CHROMIUM DRIVER" #need to put r to make the string raw so that :\Users can get passed
+    driver = webdriver.Chrome(file_path)
+
+    followers_list =[]
+
+    followers_list.append([getTwitterFollowers(driver)])
+    followers_list.append([getInstaFollowers(driver)])
+    followers_list.append([getTikTokFollowers(driver)])
+
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    SERVICE_ACCOUNT_FILE = r'#ABSOLUTE PATH OF KEYS(SERVICE ACCOUNT KEY).JSON FILE'
+
+    # set the creds and create the creds for the service acc
+    creds = None
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # The ID and range of a sample spreadsheet.
+    SAMPLE_SPREADSHEET_ID = '#CAN BE FOUND IN BETWEEN /d AND /edit#gid=0 IN URL OF WEB BROWSER'
+    SAMPLE_RANGE_NAME = '#SHEETNAME!(CELL):(CELL)'
+
+    # create the build for the spreadsheeet
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                   range=SAMPLE_RANGE_NAME, valueInputOption="USER_ENTERED",
+                                   body={"values": followers_list}).execute()
+    print(result)
+    driver.close()
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
